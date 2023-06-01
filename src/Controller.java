dcd710b6a00fbadeb6d07ebefeda4fbda13e7d03
natistuomi <77719270+natistuomi@users.noreply.github.com>
2023-06-01 09:29:14 +0200

    Error clean options

--- src/Controller.java
+++ src/Controller.java
@@ -13,24 +13,28 @@ public class Controller {
             }
         });
         view.start();
+        playGame();
+    }
+
+    public void playGame(){
+        while(model.bothKingsAlive()){
+            //player's turn loop
+            while(view.isNoChange()){}
+            view.resetNoChange();
+            if(model.checkIfSquareIsViable(view.getSelected())){
+                view.setOptions(model.getOptions(view.getSelected()));
+            }
+            //else if false... check if move option
+                // piece moves (removed from square, moved to square and possible delete of other piece)
+                // model.moveDone();
+                // view.setMoves(m.getMoves);
+        }
+        //model.sendScore(view.get(players), model.getWinner());
     }
 
     public static void main(String[] args) {
         Model m = new Model();
         View v = new View(m.getSquare());
         Controller c = new Controller(m, v);
-        while(m.bothKingsAlive()){
-            //player's turn loop
-            while(v.isNoChange()){}
-            v.resetNoChange();
-            if(m.checkIfSquareIsViable(v.getSelected())){
-                v.setOptions(m.getOptions(v.getSelected()));
-            }
-            //else if false... check if move option
-                // piece moves (removed from square, moved to square and possible delete of other piece)
-                // m.moveDone();
-                // v.setMoves(m.getMoves);
-        }
-        //m.sendScore(v.get(players), m.getWinner());
     }
 }
